1660abaa8107c48ae700e95939ed42a1d0437940
nguyen.van.ngoc <nguyen.van.ngoc@vareal.vn>
2025-04-16 15:31:46 +0700

    Refactor schema

---
+++ app/graphql/types/membership_status_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  class MembershipStatusType < Types::BaseEnum
+    description "Membership status enum"
+
+    value "PENDING", "The membership is pending", value: :pending
+    value "ACCEPTED", "The membership is accepted", value: :accepted
+    value "REJECTED", "The membership is rejected", value: :rejected
+  end
+end
